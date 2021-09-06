7658ca566528f6d78ec067c41af37de20e886a6b
Carbonera <m.k.c.carbonera@gmail.com>
2021-09-06 14:56:13 -0300

    feat: upgrate version dotenv and adjust app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/./../.env" });
+
 // rest of the code remains same
 const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+const PORT = process.env.PORT ?? 8080;
+app.get("/", (req, res) => res.send(`Welcome to ${process.env.APP_NAME}`));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
